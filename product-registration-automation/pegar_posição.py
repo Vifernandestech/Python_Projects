c7eb2bc03e5c24efe4be4ddc387fc89163901a0a
Vifernandestech <vifernandes.tech@gmail.com>
2025-01-21 22:37:17 -0300

    Adicionado código (script) pegar_posição.py

---
+++ product-registration-automation/pegar_posição.py
@@ -0,0 +1,8 @@
+import time
+import pyautogui
+
+# Tempo para posicionar o mouse no local correto.
+time.sleep(8) # Tempo que aguardará antes de executar o script. 
+
+#Comando print que mostrará as coordenadas. 
+print(pyautogui.position()) 
